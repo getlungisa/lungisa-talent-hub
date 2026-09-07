d3aab1c50fd49594c351c294949353d5073b4e38
riseventurelab <neil@riseventurelab.co>
2026-09-07 21:00:19 +0100

    Delete src/lungisa/components/TopUpModal.tsx

--- src/lungisa/components/TopUpModal.tsx
+++
@@ -1,4 +0,0 @@
-export function TopUpModal(_: { open?: boolean; onOpenChange?: (v: boolean) => void } = {}) {
-  // Top-up modal removed as part of the interview credits feature removal.
-  return null;
-}
